de7a40a2f69b6a556148e0fa238dd1f91c892bd8
agcheb <agchebykin@gmail.com>
2018-03-22 10:46:06 +0300

    в фрагмент главной активности добавлен recyclerview с gridlayout (временно добавлено несколько картинок для отображения расположения плиток)

--- app/src/main/java/com/agcheb/instagramclient/MainActivityFragment.java
+++ app/src/main/java/com/agcheb/instagramclient/MainActivityFragment.java
@@ -4,14 +4,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    RecyclerView rv;
 
     public MainActivityFragment() {
     }
@@ -24,14 +32,21 @@ public class MainActivityFragment extends Fragment {
 
          View rootview = inflater.inflate(R.layout.fragment_main, container, false);
 
-//        FloatingActionButton fab = rootview.findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Фото добавлено", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+         List<PhotoCard> modelPhotos = new ArrayList<>();
+         int imgid;
+        for (int i = 0; i < 6; i++) {
+            if(i%2==0)imgid = R.drawable.snow;
+            else imgid = R.drawable.sun;
+            modelPhotos.add(new PhotoCard(imgid));
+        }
+
+
+        rv = rootview.findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(),2);
+        rv.setLayoutManager(layoutManager);
+        RVAdapter rvAdapter = new RVAdapter(modelPhotos);
+        rv.setAdapter(rvAdapter);
         return rootview;
     }
+
 }
---
+++ app/src/main/java/com/agcheb/instagramclient/PhotoCard.java
@@ -0,0 +1,17 @@
+package com.agcheb.instagramclient;
+
+/**
+ * Created by agcheb on 20.03.18.
+ */
+
+public class PhotoCard {
+    private int mImageID;
+
+    public PhotoCard(int mImageID) {
+        this.mImageID = mImageID;
+    }
+
+    public int getmImageID() {
+        return mImageID;
+    }
+}
